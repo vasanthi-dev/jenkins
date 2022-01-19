ce3af379d4113758df751555284a6ca65ee8c020
vasanthi-dev <vasanthi2dev@gmail.com>
2022-01-19 11:04:22 -0500

    ansible seed job

--- seed.groovy
+++ seed.groovy
@@ -1,16 +1,20 @@
-job('example') {
-    logRotator(-1, 10)
-    jdk('Java 8')
-    scm {
-        github('jenkinsci/job-dsl-plugin', 'master')
+pipelineJob('roboshop-ansible') {
+  configure { flowdefinition ->
+    flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
+      'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
+        'userRemoteConfigs' {
+          'hudson.plugins.git.UserRemoteConfig' {
+            'url'('https://github.com/vasanthi-dev/AnsibleRoboshop')
+          }
+        }
+        'branches' {
+          'hudson.plugins.git.BranchSpec' {
+            'name'('*/main')
+          }
+        }
+      }
+      'scriptPath'('Jenkinsfile')
+      'lightweight'(true)
     }
-    triggers {
-        githubPush()
-    }
-    steps {
-        gradle('clean build')
-    }
-    publishers {
-        archiveArtifacts('job-dsl-plugin/build/libs/job-dsl.hpi')
-    }
+  }
 }
