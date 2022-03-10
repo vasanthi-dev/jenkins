02cc08595a1dd19ad8d90e13767196eb9a6e6589
vasanthi-dev <vasanthi2dev@gmail.com>
2022-03-10 14:27:18 -0500

    sample seed job

--- seed.groovy
+++ seed.groovy
@@ -1,23 +1,23 @@
-pipelineJob('roboshop-ansible') {
-  configure { flowdefinition ->
-    flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
-      'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
-        'userRemoteConfigs' {
-          'hudson.plugins.git.UserRemoteConfig' {
-            'url'('https://github.com/vasanthi-dev/AnsibleRoboshop')
-          }
-        }
-        'branches' {
-          'hudson.plugins.git.BranchSpec' {
-            'name'('*/main')
-          }
-        }
-      }
-      'scriptPath'('Jenkinsfile')
-      'lightweight'(true)
-    }
-  }
-}
+//pipelineJob('roboshop-ansible') {
+//  configure { flowdefinition ->
+//    flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
+//      'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
+//        'userRemoteConfigs' {
+//          'hudson.plugins.git.UserRemoteConfig' {
+//            'url'('https://github.com/vasanthi-dev/AnsibleRoboshop')
+//          }
+//        }
+//        'branches' {
+//          'hudson.plugins.git.BranchSpec' {
+//            'name'('*/main')
+//          }
+//        }
+//      }
+//      'scriptPath'('Jenkinsfile')
+//      'lightweight'(true)
+//    }
+//  }
+//}
 
 
 //job('example') {
@@ -36,7 +36,7 @@ pipelineJob('roboshop-ansible') {
 //    archiveArtifacts('job-dsl-plugin/build/libs/job-dsl.hpi')
 //  }
 //}
-
+//
 pipeline {
   agent any
   environment {
