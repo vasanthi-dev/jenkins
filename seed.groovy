148bcb154183b3d966e192467920f301c01024aa
vasanthi-dev <vasanthi2dev@gmail.com>
2022-03-11 10:39:32 -0500

    Jenkinsfile with folder script

--- seed.groovy
+++ seed.groovy
@@ -1,24 +1,3 @@
-////pipelineJob('frontend') {
-////  configure { flowdefinition ->
-////    flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
-////      'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
-////        'userRemoteConfigs' {
-////          'hudson.plugins.git.UserRemoteConfig' {
-////            'url'('https://DevOps-Batches@dev.azure.com/DevOps-Batches/DevOps60/_git/frontend')
-////          }
-////        }
-////        'branches' {
-////          'hudson.plugins.git.BranchSpec' {
-////            'name'('*/main')
-////          }
-////        }
-////      }
-////      'scriptPath'('Jenkinsfile')
-////      'lightweight'(true)
-////    }
-////  }
-////}
-//
 //pipelineJob('roboshop-ansible') {
 //  configure { flowdefinition ->
 //    flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
@@ -39,235 +18,29 @@
 //    }
 //  }
 //}
-//
-//folder('CI-Pipelines') {
-//  displayName('CI-Pipelines')
-//  description('CI-Pipelines')
-//}
-//
-//  pipelineJob("CI-Pipelines/${j}") {
-//    configure { flowdefinition ->
-//      flowdefinition << delegate.'definition'(class: 'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition', plugin: 'workflow-cps') {
-//        'scm'(class: 'hudson.plugins.git.GitSCM', plugin: 'git') {
-//          'userRemoteConfigs' {
-//            'hudson.plugins.git.UserRemoteConfig' {
-//              'url'("https://github.com/raghudevopsb61/${j}.git")
-//            }
-//          }
-//          'branches' {
-//            'hudson.plugins.git.BranchSpec' {
-//              'name'('*/tags/*')
-//            }
-//            'hudson.plugins.git.BranchSpec' {
-//              'name'('*/main')
-//            }
-//          }
-//        }
-//        'scriptPath'('Jenkinsfile')
-//        'lightweight'(true)
-//      }
-//    }
-//  }
-//}
-//
-//folder('Mutable') {
-//  displayName('Mutable')
-//  description('Mutable')
-//}
-//
-//pipelineJob('Mutable/VPC') {
-//  configure { flowdefinition ->
-//    flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
-//      'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
-//        'userRemoteConfigs' {
-//          'hudson.plugins.git.UserRemoteConfig' {
-//            'url'('https://github.com/raghudevopsb61/terraform-vpc.git')
-//          }
-//        }
-//        'branches' {
-//          'hudson.plugins.git.BranchSpec' {
-//            'name'('*/main')
-//          }
-//        }
-//      }
-//      'scriptPath'('Jenkinsfile')
-//      'lightweight'(true)
-//    }
-//  }
-//}
-//
-//pipelineJob('Mutable/APP-SETUP') {
-//  configure { flowdefinition ->
-//    flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
-//      'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
-//        'userRemoteConfigs' {
-//          'hudson.plugins.git.UserRemoteConfig' {
-//            'url'('https://github.com/raghudevopsb61/jenkins.git')
-//          }
-//        }
-//        'branches' {
-//          'hudson.plugins.git.BranchSpec' {
-//            'name'('*/main')
-//          }
-//        }
-//      }
-//      'scriptPath'('Jenkinsfile-Mutable-Infra')
-//      'lightweight'(true)
-//    }
-//  }
-//}
-//
-//pipelineJob('Mutable/ALB') {
-//  configure { flowdefinition ->
-//    flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
-//      'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
-//        'userRemoteConfigs' {
-//          'hudson.plugins.git.UserRemoteConfig' {
-//            'url'('https://github.com/raghudevopsb61/terrafrom-mutable-alb.git')
-//          }
-//        }
-//        'branches' {
-//          'hudson.plugins.git.BranchSpec' {
-//            'name'('*/main')
-//          }
-//        }
-//      }
-//      'scriptPath'('Jenkinsfile')
-//      'lightweight'(true)
-//    }
-//  }
-//}
-//
-//pipelineJob('Mutable/DB') {
-//  configure { flowdefinition ->
-//    flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
-//      'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
-//        'userRemoteConfigs' {
-//          'hudson.plugins.git.UserRemoteConfig' {
-//            'url'('git@github.com:raghudevopsb61/terraform-db.git')
-//          }
-//        }
-//        'branches' {
-//          'hudson.plugins.git.BranchSpec' {
-//            'name'('*/main')
-//          }
-//        }
-//      }
-//      'scriptPath'('Jenkinsfile')
-//      'lightweight'(true)
-//    }
-//  }
-//}
-//
-//
-//pipelineJob('Mutable/All-Infra-Create') {
-//  configure { flowdefinition ->
-//    flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
-//      'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
-//        'userRemoteConfigs' {
-//          'hudson.plugins.git.UserRemoteConfig' {
-//            'url'('https://github.com/raghudevopsb61/jenkins.git')
-//          }
-//        }
-//        'branches' {
-//          'hudson.plugins.git.BranchSpec' {
-//            'name'('*/main')
-//          }
-//        }
-//      }
-//      'scriptPath'('Jenkinsfile-Mutable-All-in-one-Infra-Create')
-//      'lightweight'(true)
-//    }
-//  }
-//}
-//
-//pipelineJob('Mutable/All-Infra-Destroy') {
-//  configure { flowdefinition ->
-//    flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
-//      'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
-//        'userRemoteConfigs' {
-//          'hudson.plugins.git.UserRemoteConfig' {
-//            'url'('https://github.com/raghudevopsb61/jenkins.git')
-//          }
-//        }
-//        'branches' {
-//          'hudson.plugins.git.BranchSpec' {
-//            'name'('*/main')
-//          }
-//        }
-//      }
-//      'scriptPath'('Jenkinsfile-Mutable-All-in-one-Infra-Destroy')
-//      'lightweight'(true)
-//    }
-//  }
-//}
-//
-///////////////////////
-//
-//folder('Immutable') {
-//  displayName('Immutable')
-//  description('Immutable')
-//}
-//
-//pipelineJob('Immutable/All-Infra-Create') {
-//  configure { flowdefinition ->
-//    flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
-//      'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
-//        'userRemoteConfigs' {
-//          'hudson.plugins.git.UserRemoteConfig' {
-//            'url'('https://github.com/raghudevopsb61/jenkins.git')
-//          }
-//        }
-//        'branches' {
-//          'hudson.plugins.git.BranchSpec' {
-//            'name'('*/main')
-//          }
-//        }
-//      }
-//      'scriptPath'('Jenkinsfile-Immutable-All-in-one-Infra-Create')
-//      'lightweight'(true)
-//    }
-//  }
-//}
-//
-//pipelineJob('Immutable/All-Infra-Destroy') {
-//  configure { flowdefinition ->
-//    flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
-//      'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
-//        'userRemoteConfigs' {
-//          'hudson.plugins.git.UserRemoteConfig' {
-//            'url'('https://github.com/raghudevopsb61/jenkins.git')
-//          }
-//        }
-//        'branches' {
-//          'hudson.plugins.git.BranchSpec' {
-//            'name'('*/main')
-//          }
-//        }
-//      }
-//      'scriptPath'('Jenkinsfile-Immutable-All-in-one-Infra-Destroy')
-//      'lightweight'(true)
-//    }
-//  }
-//}
-//
-//pipelineJob('Immutable/App-Deploy') {
-//  configure { flowdefinition ->
-//    flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
-//      'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
-//        'userRemoteConfigs' {
-//          'hudson.plugins.git.UserRemoteConfig' {
-//            'url'('https://github.com/raghudevopsb61/jenkins.git')
-//          }
-//        }
-//        'branches' {
-//          'hudson.plugins.git.BranchSpec' {
-//            'name'('*/main')
-//          }
-//        }
-//      }
-//      'scriptPath'('Jenkinsfile-Immutable-App-Deploy')
-//      'lightweight'(true)
-//    }
-//  }
-//}
+
+folder('CI-Pipelines') {
+  displayName('CI-Pipelines')
+  description('CI-Pipelines')
+}
+
+pipelineJob('CI-Pipelines/cart') {
+    configure { flowdefinition ->
+        flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
+            'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
+                'userRemoteConfigs' {
+                    'hudson.plugins.git.UserRemoteConfig' {
+                        'url'('https://github.com/vasanthi-dev/cart.git')
+                    }
+                }
+                'branches' {
+                    'hudson.plugins.git.BranchSpec' {
+                        'name'('*/main')
+                    }
+                }
+            }
+            'scriptPath'('Jenkinsfile')
+            'lightweight'(true)
+        }
+    }
+}
