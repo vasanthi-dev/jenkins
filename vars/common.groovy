6ceae813e76282303a715885cd417d0b2e9a26fb
vasanthi-dev <vasanthi2dev@gmail.com>
2022-03-11 20:12:01 -0500

    commented triggers

--- vars/common.groovy
+++ vars/common.groovy
@@ -1,4 +1,5 @@
 def sonarQube(){
     sh 'sonar-scanner -Dsonar.projectKey=${COMPONENT} -Dsonar.sources=. -Dsonar.host.url=http://3.239.24.64:9000 -Dsonar.login=admin -Dsonar.password=admin123'
     sh 'sonar-quality-gate.sh admin admin123 3.239.24.64 ${COMPONENT}'
+    println "sonarQube Testing"
 }
