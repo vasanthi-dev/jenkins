aafee9be5db1c05f28a3925a0dbe19d3ae2faac1
vasanthi-dev <vasanthi2dev@gmail.com>
2022-03-14 04:14:34 -0400

    artifact pushing into nexus

--- vars/common.groovy
+++ vars/common.groovy
@@ -18,7 +18,7 @@ def publishArtifacts() {
 //    }
 
     sh '''
-    curl -v -u ${NEXUS_USR}:${NEXUS_PSW} --upload-file ${COMPONENT}-${gitTag}.zip http://nexus.roboshop.internal:8081/repository/${COMPONENT}/${COMPONENT}-${gitTag}.zip
+    curl -f -v -u ${NEXUS_USR}:${NEXUS_PSW} --upload-file ${COMPONENT}-${gitTag}.zip http://nexus.roboshop.internal:8081/repository/${COMPONENT}/${COMPONENT}-${gitTag}.zip
     '''
 
 }
--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -39,9 +39,9 @@ def call() {
                 }
             }
             stage('Publish Artifacts') {
-//                when {
-//                    expression { sh([returnStdout: true, script: 'echo ${GIT_BRANCH} | grep tags || true' ]) }
-//                }
+                when {
+                    expression { sh([returnStdout: true, script: 'echo ${GIT_BRANCH} | grep tags || true' ]) }
+                }
                 steps {
                     script {
                         common.prepareArtifacts()
