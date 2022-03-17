16a8e5096eb3b5b08e80069db7de0b9c26fc0bff
vasanthi-dev <vasanthi2dev@gmail.com>
2022-03-17 16:20:45 -0400

    creating Amazon Load balancer

--- seed.groovy
+++ seed.groovy
@@ -98,3 +98,24 @@ pipelineJob('Mutable/APP-SETUP') {
         }
     }
 }
+
+pipelineJob('Mutable/ALB') {
+    configure { flowdefinition ->
+        flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
+            'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
+                'userRemoteConfigs' {
+                    'hudson.plugins.git.UserRemoteConfig' {
+                        'url'('https://github.com/vasanthi-dev/terraform-mutable-alb.git')
+                    }
+                }
+                'branches' {
+                    'hudson.plugins.git.BranchSpec' {
+                        'name'('*/main')
+                    }
+                }
+            }
+            'scriptPath'('Jenkinsfile')
+            'lightweight'(true)
+        }
+    }
+}
