4cf4ecb21b16ba51e30a1402f8fb308f4f3e05c9
vasanthi-dev <vasanthi2dev@gmail.com>
2022-03-24 04:58:04 -0400

    Elasticache service in amazon

--- seed.groovy
+++ seed.groovy
@@ -141,3 +141,25 @@ pipelineJob('Mutable/DB') {
         }
     }
 }
+
+
+pipelineJob('Mutable/All-Infra-Create') {
+    configure { flowdefinition ->
+        flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
+            'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
+                'userRemoteConfigs' {
+                    'hudson.plugins.git.UserRemoteConfig' {
+                        'url'('https://github.com/vasanthi-dev/jenkins.git')
+                    }
+                }
+                'branches' {
+                    'hudson.plugins.git.BranchSpec' {
+                        'name'('*/main')
+                    }
+                }
+            }
+            'scriptPath'('Jenkinsfile-Mutable-All-In-one-Infra-create')
+            'lightweight'(true)
+        }
+    }
+}
