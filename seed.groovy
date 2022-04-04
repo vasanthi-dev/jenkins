09a8cbba545bb3146d704791e3da9626e52cc888
vasanthi-dev <vasanthi2dev@gmail.com>
2022-04-04 10:47:36 -0400

    jenkins immutable update

--- seed.groovy
+++ seed.groovy
@@ -235,3 +235,24 @@ pipelineJob('Immutable/All-Infra-Destroy') {
         }
     }
 }
+
+pipelineJob('Immutable/App-Deploy') {
+    configure { flowdefinition ->
+        flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
+            'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
+                'userRemoteConfigs' {
+                    'hudson.plugins.git.UserRemoteConfig' {
+                        'url'('https://github.com/vasanthi-dev/jenkins.git')
+                    }
+                }
+                'branches' {
+                    'hudson.plugins.git.BranchSpec' {
+                        'name'('*/main')
+                    }
+                }
+            }
+            'scriptPath'('Jenkinsfile-Immutable-App_Deploy')
+            'lightweight'(true)
+        }
+    }
+}
